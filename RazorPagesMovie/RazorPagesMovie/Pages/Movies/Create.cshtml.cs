009559613686b0a0ae8f1acf32123e205bd4b607
LarsHolwerda <lardy.holwerda@gmail.com>
2023-07-22 16:13:32 +0200

    initial commit

---
+++ RazorPagesMovie/RazorPagesMovie/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+
+namespace RazorPagesMovie.Pages.Movies
+{
+    public class CreateModel : PageModel
+
+
+    {
+        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+        public CreateModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; } = default!;
+
+        static HttpClient client = new HttpClient();
+        static async Task<IActionResult> CreateMovieAsync(Movie Movie)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "https://ichtus-web-api.azurewebsites.net/api/Events", Movie);
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return new OkObjectResult(Movie);
+        }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Movie == null || Movie == null)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
